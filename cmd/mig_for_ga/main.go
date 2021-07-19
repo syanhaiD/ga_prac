e88670798fa037741a6eebb665b42593287b7b6e
holy_road_ss <lolilysuzume893@gmail.com>
2021-07-19 06:40:13 +0000

    mod workflow

--- cmd/mig_for_ga/main.go
+++ cmd/mig_for_ga/main.go
@@ -15,12 +15,16 @@ func main() {
 	p, _ := os.Getwd()
 	splitPath := strings.Split(p, projectName)
 	dbTomlPath := fmt.Sprintf("%v%v/cmd/mig_for_ga/database.toml", splitPath[0], projectName)
+	dbTomlPath = fmt.Sprintf("cmd/mig_for_ga/database.toml")
 	rdb.DatabaseTomlPath = dbTomlPath
 	rdb.Connect()
 	sqlFilesPath := fmt.Sprintf("%v%v/pkg/rdb/sqlfiles/", splitPath[0], projectName)
+	sqlFilesPath = fmt.Sprintf("pkg/rdb/sqlfiles/")
 	rdb.SqlFilesPath = sqlFilesPath
 	schemaTomlPath := fmt.Sprintf("%v%v/pkg/rdb/schema.toml", splitPath[0], projectName)
+	schemaTomlPath = fmt.Sprintf("pkg/rdb/schema.toml")
 	connTomlPath := fmt.Sprintf("%v%v/cmd/mig_for_ga/conn.toml", splitPath[0], projectName)
+	connTomlPath = fmt.Sprintf("cmd/mig_for_ga/conn.toml")
 	err := gomig.Exec(schemaTomlPath, env, connTomlPath, false)
 	if err != nil {
 		fmt.Println(err)
