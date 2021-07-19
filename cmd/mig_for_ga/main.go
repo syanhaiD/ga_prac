6e0223b158e439c63030a859dbeb53b1ef82d256
holy_road_ss <lolilysuzume893@gmail.com>
2021-07-19 06:44:34 +0000

    mod workflow

--- cmd/mig_for_ga/main.go
+++ cmd/mig_for_ga/main.go
@@ -5,26 +5,19 @@ import (
 	"github.com/syanhaiD/ga_prac/pkg/rdb"
 	gomig "github.com/syanhaiD/gomig/pkg/proc"
 	"os"
-	"strings"
 )
 
 func main() {
-	projectName := "ga_prac"
 	env := "test"
 
-	p, _ := os.Getwd()
-	splitPath := strings.Split(p, projectName)
-	dbTomlPath := fmt.Sprintf("%v%v/cmd/mig_for_ga/database.toml", splitPath[0], projectName)
-	dbTomlPath = fmt.Sprintf("cmd/mig_for_ga/database.toml")
+	// github_actionsの場合はPJルートからの相対パスで指定
+	dbTomlPath := fmt.Sprintf("cmd/mig_for_ga/database.toml")
 	rdb.DatabaseTomlPath = dbTomlPath
 	rdb.Connect()
-	sqlFilesPath := fmt.Sprintf("%v%v/pkg/rdb/sqlfiles/", splitPath[0], projectName)
-	sqlFilesPath = fmt.Sprintf("pkg/rdb/sqlfiles/")
+	sqlFilesPath := fmt.Sprintf("pkg/rdb/sqlfiles/")
 	rdb.SqlFilesPath = sqlFilesPath
-	schemaTomlPath := fmt.Sprintf("%v%v/pkg/rdb/schema.toml", splitPath[0], projectName)
-	schemaTomlPath = fmt.Sprintf("pkg/rdb/schema.toml")
-	connTomlPath := fmt.Sprintf("%v%v/cmd/mig_for_ga/conn.toml", splitPath[0], projectName)
-	connTomlPath = fmt.Sprintf("cmd/mig_for_ga/conn.toml")
+	schemaTomlPath := fmt.Sprintf("pkg/rdb/schema.toml")
+	connTomlPath := fmt.Sprintf("cmd/mig_for_ga/mig.toml")
 	err := gomig.Exec(schemaTomlPath, env, connTomlPath, false)
 	if err != nil {
 		fmt.Println(err)
