216883914e34ed3a294ed7fd1e16ec9ad502f138
holy_road_ss <lolilysuzume893@gmail.com>
2021-07-19 07:09:48 +0000

    mod

--- all_test.sh
+++ all_test.sh
@@ -4,13 +4,6 @@ set -e
 
 go clean -testcache
 
-text='
-aaa
-bbb
-ccc
-'
-array_text=(`echo $text`)
-
 TEST_TARGETS=($(go list ./...))
 for target in "${TEST_TARGETS[@]}"; do
     go test ${target}
